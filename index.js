e069508e5f14263c6d831a30e9f61b5582db081a
Dev-SaadKamal <saadkamal937@gmail.com>
2026-04-18 14:18:48 +0500

    cors

--- index.js
+++ index.js
@@ -15,10 +15,26 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: "https://aprilfinalhackathon.netlify.app/",
-    credentials: true
-}));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://aprilfinalhackathon.netlify.app"
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            } else {
+                return callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true
+    })
+);
+
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
