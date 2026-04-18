0f31d5f5bb765c14f9681a6cbe3d4b74fa28e5bf
Dev-SaadKamal <saadkamal937@gmail.com>
2026-04-18 14:35:49 +0500

    db

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected successfully');
-    }
-    catch (err) {
-        console.error('MongoDB connection error:', err);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error("MongoDB connection error ❌", err);
+        process.exit(1); // VERY IMPORTANT
     }
 };
 
